07df1eddd1c3259352ddae92401ee80d9071570f
vasiioandre <vasi_ioandre@yahoo.com>
2024-06-12 21:29:52 +0300

    Move React State up to have a global conuter button

--- src/App.js
+++ src/App.js
@@ -4,30 +4,9 @@ import Counter from './components/counter/Counter';
 function App() {
   return (
     <div className="App">
-      {/* <PlayingWithProps property1="value1" property2="value2" /> */}
       <Counter />
-      <Counter by={2} />
-      <Counter by={3} />
     </div>
   );
-}
-
-// function PlayingWithProps(properties) {
-//   console.log(properties)
-//   console.log(properties.property1)
-
-//   return (
-//     <div>Props</div>
-//   )
-// }
-
-function PlayingWithProps({property1, property2}) {
-  //console.log(properties.property1)
-  console.log(property2)
-
-  return (
-    <div>Props</div>
-  )
 }
 
 export default App;
--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -2,48 +2,64 @@ import { useState } from 'react'
 import { PropTypes } from 'prop-types'
 import './Counter.css'
 
-export default function Counter({by}) {
+export default function Counter() {
+  const [count, setCount] = useState(0);
 
-    //const state = useState(0);
-    const [count, setCount] = useState(0);
+  function incrementCounterParentFunction(by) {
+    setCount(count + by)
+  }
 
-    console.log(by)
+  function decrementCounterParentFunction(by) {
+    setCount(count - by)
+  }
 
-    function incrementCounterFunction() {
-        // console.log(state)
-        // console.log(state[0])
-        // console.log(state[1])
-        // state[1](state[0] + 1)
-
-        setCount(count + by)
-        console.log(count)
-    }
-
-    function decrementCounterFunction() {
-        setCount(count - by)
-    }
-
-    return (
-        <div className="Count">
-            <span className="count">{count}</span>
-            <div>
-                <button className="counterButton"
-                    onClick={incrementCounterFunction}
-                >+{by}</button>
-            </div>
-            <div>
-                <button className="counterButton"
-                    onClick={decrementCounterFunction}
-                >-{by}</button>
-            </div>
-        </div>
-    )
+  return (
+    <>
+      <span className="totalCount">{count}</span>
+      <CounterButton by={1}
+        incrementMethod={incrementCounterParentFunction}
+        decrementMethod={decrementCounterParentFunction}/>
+      <CounterButton by={2}
+      incrementMethod={incrementCounterParentFunction}
+      decrementMethod={decrementCounterParentFunction}/>
+      <CounterButton by={3}
+      incrementMethod={incrementCounterParentFunction}
+      decrementMethod={decrementCounterParentFunction}/>
+    </>
+  )
 }
 
-Counter.propTypes = {
-    by: PropTypes.number
+function CounterButton({by, incrementMethod, decrementMethod}) {
+
+  //const state = useState(0);
+  const [count, setCount] = useState(0);
+
+  console.log(by)
+
+  function incrementCounterFunction() {
+    setCount(count + by)
+    incrementMethod(by)
+  }
+
+  function decrementCounterFunction() {
+    setCount(count - by)
+    decrementMethod(by)
+  }
+
+  return (
+    <div className="Count">
+      <div>
+        <button className="counterButton"
+            onClick={incrementCounterFunction}
+        >+{by}</button>
+        <button className="counterButton"
+            onClick={decrementCounterFunction}
+        >-{by}</button>
+      </div>
+    </div>
+  )
 }
 
-Counter.defaultProps = {
-    by: 1
+CounterButton.propTypes = {
+  by: PropTypes.number
 }
