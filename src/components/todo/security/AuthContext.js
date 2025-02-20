6dff034131c675927291d1f6056bc881eb80e50c
vasiioandre <vasi_ioandre@yahoo.com>
2025-02-20 20:26:15 +0200

    Setting up Axios Interceptor to add Authorization Header

--- src/components/todo/security/AuthContext.js
+++ src/components/todo/security/AuthContext.js
@@ -1,5 +1,6 @@
 import { createContext, useContext, useState } from "react";
 import { executeBasicAuthenticationService } from "../api/HelloWorldApiService";
+import { apiClient } from "../api/ApiClient";
 
 export const AuthContext = createContext()
 
@@ -20,6 +21,15 @@ export default function AuthProvider({ children }) {
         setAuthenticated(true)
         setUsername(username)
         setToken(baToken)
+
+        apiClient.interceptors.request.use(
+          (config) => {
+            console.log('intercepting and adding a token')
+            config.headers.Authorization=baToken
+            return config
+          }
+        )
+
         return true
       } else {
         logout()
